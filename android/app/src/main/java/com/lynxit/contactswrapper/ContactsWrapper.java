072ba3db3d21894ce4d78489ab8b234f5e575615
William <williamliang82@yahoo.com>
2018-11-26 14:30:39 +0700

    Merged Android change

--- android/app/src/main/java/com/lynxit/contactswrapper/ContactsWrapper.java
+++ android/app/src/main/java/com/lynxit/contactswrapper/ContactsWrapper.java
@@ -28,6 +28,7 @@ import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReadableMapKeySetIterator;
 import com.facebook.react.bridge.ReadableType;
 import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.uimanager.ViewManager;
 
 public class ContactsWrapper extends ReactContextBaseJavaModule implements ActivityEventListener {
@@ -58,12 +59,15 @@ public class ContactsWrapper extends ReactContextBaseJavaModule implements Activ
         add(ContactsContract.CommonDataKinds.Email.TYPE);
         add(ContactsContract.CommonDataKinds.Email.LABEL);
     }};
+    private Context context;
 
 
     public ContactsWrapper(ReactApplicationContext reactContext) {
         super(reactContext);
         this.contentResolver = getReactApplicationContext().getContentResolver();
         reactContext.addActivityEventListener(this);
+        this.context = reactContext;
+
     }
 
     @Override
@@ -125,7 +129,12 @@ public class ContactsWrapper extends ReactContextBaseJavaModule implements Activ
                             //First get ID
                             String id = null;
                             int idx;
+
                             WritableMap contactData = Arguments.createMap();
+                            WritableArray phonesArray = Arguments.createArray();
+                            WritableArray emailsArray = Arguments.createArray();
+
+
                             Cursor cursor = this.contentResolver.query(contactUri, null, null, null, null);
                             if (cursor != null && cursor.moveToFirst()) {
                                 idx = cursor.getColumnIndex(ContactsContract.Contacts._ID);
@@ -155,9 +164,71 @@ public class ContactsWrapper extends ReactContextBaseJavaModule implements Activ
                             /* Map Any contact data we want returned to the JS object key for React Native */
                             HashMap<String, String> returnKeys = new HashMap<String, String>();
                             returnKeys.put(ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE, "name");
-                            returnKeys.put(ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE, "phone");
-                            returnKeys.put(ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE, "email");
 
+                            // loops through all phones
+                            
+                            Cursor phones = this.contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + id, null, null);
+                            while (phones.moveToNext())
+                            {
+                                WritableMap phoneObj = Arguments.createMap();
+                                String number = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                                int type = phones.getInt(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE));
+                                String phoneType = "";
+                                switch (type) {
+                                    case ContactsContract.CommonDataKinds.Phone.TYPE_HOME:
+                                        // do something with the Home number here..
+                                        phoneType = " home";
+                                        break;
+                                    case ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE:
+                                        // do something with the Mobile number here...
+                                        phoneType = "mobile";
+                                        break;
+                                    case ContactsContract.CommonDataKinds.Phone.TYPE_WORK:
+                                        phoneType = "work";
+                                        // do something with the Work number here...
+                                        break;
+                                }
+                                phoneObj.putString("number", number);
+                                phoneObj.putString("number_type", phoneType);
+                                phonesArray.pushMap(phoneObj);
+                            }
+                            phones.close();
+
+                            // loops through all emails 
+                            Cursor emails = this.contentResolver.query(ContactsContract.CommonDataKinds.Email.CONTENT_URI, null,
+        ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = " + id, null, null);
+                            while (emails.moveToNext())
+                            {
+                                WritableMap emailObj = Arguments.createMap();
+                                String emailAddress = emails.getString(emails.getColumnIndex(ContactsContract.CommonDataKinds.Email.ADDRESS));
+                                int type = emails.getInt(emails.getColumnIndex(ContactsContract.CommonDataKinds.Email.TYPE));
+                                String emailType = "";
+                                switch (type) {
+                                    case ContactsContract.CommonDataKinds.Email.TYPE_HOME:
+                                        // do something with the Home number here..
+                                        emailType = "home";
+                                        break;
+                                    case ContactsContract.CommonDataKinds.Email.TYPE_MOBILE:
+                                        // do something with the Mobile number here...
+                                        emailType = "mobile";
+                                        break;
+                                    case ContactsContract.CommonDataKinds.Email.TYPE_WORK:
+                                        emailType = "work";
+                                        // do something with the Work number here...
+                                        break;
+                                }
+                                Log.w(">>>>>>>>>>>>>>>>>>>>>>>>>>>>", emailType);
+                                emailObj.putString("address", emailAddress);
+                                emailObj.putString("address_type", emailType);
+                                emailsArray.pushMap(emailObj);
+                            }
+                            emails.close();
+
+                            contactData.putArray("emails", emailsArray);
+                            contactData.putArray("phones", phonesArray);
+
+                            // this now only grabs the name of the contact
                             int dataIdx = cursor.getColumnIndex(ContactsContract.Contacts.Entity.DATA1);
                             int mimeIdx = cursor.getColumnIndex(ContactsContract.Contacts.Entity.MIMETYPE);
                             if (cursor.moveToFirst()) {
@@ -172,6 +243,7 @@ public class ContactsWrapper extends ReactContextBaseJavaModule implements Activ
 
                             cursor.close();
                             if(foundData) {
+                                // send contact back
                                 mContactsPromise.resolve(contactData);
                                 return;
                             } else {
